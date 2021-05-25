993b71d0a754bddc10a1c8539c5a26b263fea9ba
farid <farid-n@auto.kz>
2021-05-25 16:51:38 +0600

    Feature: Added new arg in Proxy.Login = domain (DomainType) && and changed Types in arg

--- oauthProxy/login.go
+++ oauthProxy/login.go
@@ -1,10 +1,15 @@
 package oauthProxy
 
-func (p *Proxy) Login(username, password string) (string, map[string]interface{}, error) {
+type UsernameType string
+type PasswordType string
+type DomainType string
+
+func (p *Proxy) Login(username UsernameType, password PasswordType, domain DomainType) (string, map[string]interface{}, error) {
 	dto := make(map[string]interface{})
 
 	dto["username"] = username
 	dto["password"] = password
+	dto["domain"] = domain
 
 	// Getting new JWT with User Payload
 	newAuthData, userPayload, err := p.Client.Auth(dto, p.OAuthGrantType)
